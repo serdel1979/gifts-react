9ed8c948562dcbe34a2a22e4a3a934c24e72a83b
Sergio De Luca <sdlbsso@gmail.com>
2022-11-24 16:01:54 -0300

    listo - era otro endpoint

--- src/helpers/getGif.js
+++ src/helpers/getGif.js
@@ -1,5 +1,6 @@
 export const getGifs = async (category)=>{
- const url = `https://api.giphy.com/v1/gifs/trending?api_key=ecmrwi35hOcaLgIurBgZEQCk895WCMXK&q=${category}&limit=5`;
+    
+ const url = `https://api.giphy.com/v1/gifs/search?api_key=ecmrwi35hOcaLgIurBgZEQCk895WCMXK&q=${category}&limit=10`;
  const resp = await fetch(url);
  const { data } = await resp.json();
  
